461629a24e61c9445c6d5234f632a8527dfda450
Monjur Ahmed Akash <monjur3069@gmail.com>
2023-02-22 16:24:59 +0600

    Spinner and Radio Button Updated

--- NewProjectWithRoomDB/app/src/main/java/com/example/newprojectwithroomdb/NewScheduleFragment.kt
+++ NewProjectWithRoomDB/app/src/main/java/com/example/newprojectwithroomdb/NewScheduleFragment.kt
@@ -5,29 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import android.widget.RadioButton
 import com.example.newprojectwithroomdb.databinding.FragmentNewScheduleBinding
 
 class NewScheduleFragment : Fragment() {
 
     private lateinit var binding: FragmentNewScheduleBinding
+
+    private var from = "Dhaka"
+    private var to = "Dhaka"
+    private var busType = busTypeEconomy
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentNewScheduleBinding.inflate(inflater,container,false)
         initSpinner()
+        initBusTypeRadioGroup()
         return binding.root
+    }
+
+    private fun initBusTypeRadioGroup() {
+        binding.busTypeRG.setOnCheckedChangeListener { radioGroup, i ->
+            var rb: RadioButton = radioGroup.findViewById(i)
+            busType = rb.text.toString()
+        }
     }
 
     private fun initSpinner() {
         val adapter = ArrayAdapter<String>(
             requireActivity(),
-            android.R.layout.simple_dropdown_item_1line,
+            android.R.layout.simple_spinner_dropdown_item,
             cityList
         )
         binding.citySpinnerFrom.adapter = adapter
         binding.citySpinnerTo.adapter = adapter
+
+        binding.citySpinnerFrom.onItemSelectedListener = object : OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                from = p0?.getItemAtPosition(p2).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        binding.citySpinnerTo.onItemSelectedListener = object : OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                to = p0?.getItemAtPosition(p2).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
     }
 
 }
